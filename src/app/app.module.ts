f82aba8ed54eaf15b2cc1116526ba641080b5cb6
Innovating Concepts <iconcepts@Innovatings-MacBook-Pro.local>
2017-03-17 12:04:28 +0100

    Added nav component and conditional display of login link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import {RouterModule} from '@angular/router';
 import {HttpModule } from '@angular/http';
 import {AuthenticationService} from './authentication/index';
 import {appRoutingProviders, routing} from './app.routing';
-import {HomeComponent, LoginComponent} from './component/index';
+import {HomeComponent, LoginComponent, NavigationComponent} from './component/index';
 import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    LoginComponent
+    LoginComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/component/navigation/navigation.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {AuthenticationService} from '../../authentication/index';
+
+@Component({
+    selector: 'app-navigation',
+    templateUrl: './navigation.component.html',
+    styleUrls: ['./navigation.component.css']
+})
+export class NavigationComponent {
+
+    constructor(public authentication: AuthenticationService) {
+    }
+}
